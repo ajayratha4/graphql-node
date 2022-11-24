0f89f3a30a502ca234e52bac4a62bcadf860f43d
ajayratha4 <ajayratha4@gmail.com>
2022-11-24 16:29:48 +0530

    init graphql

--- src/app.js
+++ src/app.js
@@ -1,16 +1,57 @@
 const express = require("express");
-app = express();
+const cors = require("cors");
+const { json } = require("body-parser");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
 
-app.get("/ab", (req, res) => {
-  res.send({ res: "hello" });
-});
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
 
-// app.use((req, res, next) => {
-//   console.log("welcome to app");
-//   //   next();
-//   res.send("error");
-// });
+const typeDefs = `
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
 
-app.listen(8000, () => {
-  console.log(`Server is running on port 8000`);
-});
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const startExpress = async () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(json());
+
+  app.get("/", (req, res) => {
+    res.send("Express Server");
+  });
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+  app.listen({ port: 4000 }, () => {
+    console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+  });
+};
+
+startExpress();
